9c97aeeb90e3647a3e69c967940117f372d0fca5
luukalleman <luuk@alleman.nl>
2023-06-10 15:54:04 +0200

    youtube video added

--- langchain-ask-pdf-main/app.py
+++ langchain-ask-pdf-main/app.py
@@ -7,6 +7,75 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import find_dotenv, load_dotenv
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+import textwrap
+import os
+
+load_dotenv(find_dotenv())
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+embeddings = OpenAIEmbeddings()
+
+
+def create_db_from_youtube_video_url(video_url):
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=100)
+    docs = text_splitter.split_documents(transcript)
+
+    db = FAISS.from_documents(docs, embeddings)
+    return db
+
+
+def get_response_from_query(db, query, k=4):
+    """
+    gpt-3.5-turbo can handle up to 4097 tokens. Setting the chunksize to 1000 and k to 4 maximizes
+    the number of tokens to analyze.
+    """
+
+    docs = db.similarity_search(query, k=k)
+    docs_page_content = " ".join([d.page_content for d in docs])
+
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2)
+
+    # Template to use for the system message prompt
+    template = """
+        You are a helpful assistant that can answer questions about youtube videos 
+        based on the video's transcript: {docs}
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+        Your answers should be verbose and detailed.
+        """
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+
+    # Human question prompt
+    human_template = "Answer the following question: {question}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
+
+    response = chain.run(question=query, docs=docs_page_content)
+    response = response.replace("\n", "")
+    return response, docs
 
 
 def main():
@@ -92,12 +161,46 @@ def main():
 
             # Clear the text input box
             st.session_state.user_question = ""
+            # Add a text input for YouTube URL
+    youtube_url = st.sidebar.text_input("Enter YouTube video URL:")
+
+    # If a URL is entered, embed the video and process it
+    if youtube_url:
+        # Embed the YouTube video
+        st.video(youtube_url)
+
+        # Process the YouTube video transcript as you did in your existing code
+        db = create_db_from_youtube_video_url(youtube_url)
+
+        # Display conversation history in the sidebar
+        st.sidebar.markdown("## Conversation")
+        for role, message in st.session_state.conversation_history:
+            if role == 'You':
+                st.sidebar.markdown(
+                    f'<div style="color: #1f77b4">{role}: {message}</div>', unsafe_allow_html=True)
+            else:
+                st.sidebar.markdown(
+                    f'<div style="color: #ff7f0e">{role}: {message}</div>', unsafe_allow_html=True)
+
+        # Ask a question about the YouTube video
+        youtube_question = st.sidebar.text_input(
+            "Ask a question about the YouTube video:", value=st.session_state.user_question)
+
+        if youtube_question and st.sidebar.button('Ask YouTube Question'):
+            st.session_state.conversation_history.append(
+                ('You', youtube_question))
+
+            response, docs = get_response_from_query(db, youtube_question)
+            st.session_state.conversation_history.append(('Bot', response))
+
+            # Clear the text input box
+            st.session_state.user_question = ""
 
     st.sidebar.markdown("## Conversation")
     for role, message in st.session_state.conversation_history:
         if role == 'Bot':
             st.sidebar.markdown(
-                f'<div style="color: #6c757d;">**{role}:** {message}</div>', unsafe_allow_html=True)
+                f'<div style="color: #6c757d;">{role}: {message}</div>', unsafe_allow_html=True)
         else:
             st.sidebar.markdown(f"{role}: {message}")
 
